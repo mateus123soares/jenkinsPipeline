485dd6afb3081b3a52ace75e1998e9f04f0bac7d
Mateus Soares <mateus123soares@hotmail.com>
2021-07-15 16:01:28 -0300

    add https server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,14 @@
 const app = require('./app');
+const https = require('https')
+const fs = require('fs')
 
-app.listen(process.env.PORT,()=>{
+const SSL_KEY = fs.readFileSync('./certificates/key.pem')
+const SSL_CERT = fs.readFileSync('./certificates/cert.pem')
+
+const credentials = {key: SSL_KEY, cert: SSL_CERT};
+
+const server = https.createServer(credentials,app)
+
+server.listen(process.env.PORT,()=>{
     console.log('app listening on port', process.env.PORT);
 });
